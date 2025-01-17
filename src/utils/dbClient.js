3c4f4debd5967bd179fca30038236673eab3a6ff
Abdulloh <abdullajonmamanazirov@gmail.com>
2025-01-17 16:54:19 +0500

    add logging

--- src/utils/dbClient.js
+++ src/utils/dbClient.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require("path");
 const { Client } = require("pg");
 
 const DB_URL = process.env.DATABASE_URL;
@@ -6,5 +7,12 @@ const DB_URL = process.env.DATABASE_URL;
 const dbClient = new Client({
   connectionString: DB_URL,
 });
+
+dbClient
+  .connect()
+  .then(() => console.log("Connected to database", path.dirname()))
+  .catch((err) =>
+    console.error("Database connection error:", path.dirname(), err)
+  );
 
 module.exports = { dbClient };
