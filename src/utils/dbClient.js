c4f01fca6f2634f3333f61c7343b0272e2cc0036
Abdulloh <abdullajonmamanazirov@gmail.com>
2025-01-17 17:01:29 +0500

    fix typo in the variable

--- src/utils/dbClient.js
+++ src/utils/dbClient.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const { Client } = require("pg");
 
-const DB_URL = process.env.DATABASE_URL;
+const DB_URL = process.env.DB_URL;
 
 const dbClient = new Client({
   connectionString: DB_URL,
